4be98e3723ec70bcac9c273018decd48eff690b4
JosueSay <josuesay770@gmail.com>
2026-03-29 23:05:02 -0600

    feat(api): add local API initialization and controlled message dictionary

---
+++ pain001/api/local/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2023-2026 Sebastien Rousseau.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
